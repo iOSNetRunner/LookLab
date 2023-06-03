3a59b2a428493073065536733beacad4a60bcb29
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-06-03 17:55:41 +0300

    finisgSelectMaster

--- LookLabApp/ViewControllers/SelectedMasterViewController.swift
+++ LookLabApp/ViewControllers/SelectedMasterViewController.swift
@@ -8,24 +8,32 @@
 import UIKit
 
 final class SelectedMasterViewController: UIViewController {
+    
     struct Master {
-        let names: [String]
-        let surNames: [String]
-        let typeOfMaster: [String]
-        let experience: [String]
-        let phone: [String]
+        let experience: String
+        let phone: String
         let masterImageName: UIImage
-        let pricesPerService: [String]
+        let pricesPerService: String
         
-        var fullName: String {"\(names) \(surNames)"}
+        var fullName: String
     }
-    var master: [Master]!
+    var master: Master!
     
     @IBOutlet var imageMasterView: UIImageView!
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var contactLabel: UILabel!
+    @IBOutlet var experienceLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupInfo()
     }
-
+    private func setupInfo(){
+        navigationController?.title = master.fullName
+        imageMasterView.image = master.masterImageName
+        priceLabel.text = master.pricesPerService
+        contactLabel.text = master.phone
+        experienceLabel.text = master.experience
+    }
 }
