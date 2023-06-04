db0c3b300dc44f3c93de9a31ea9cd715687d267c
Fedorwell <115953771+Fedorwell@users.noreply.github.com>
2023-06-04 16:16:35 +0300

    Add new constant typeOfServece, change func prepare(for:)

--- LookLabApp/ViewControllers/ApplyForServiceTableViewController.swift
+++ LookLabApp/ViewControllers/ApplyForServiceTableViewController.swift
@@ -12,13 +12,14 @@ final class ApplyForServiceTableViewController: UITableViewController {
     //MARK: - Methods
     
     var masters: [Master]!
+    let typeOfServece = DataStore.shared.typeOfMaster
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
        
         tableView.dataSource = self
-        masters = Master.getMasters() // получаем список мастеров
+        //masters = Master.getMasters() // получаем список мастеров
     }
     
     // MARK: - Table view data source
@@ -29,19 +30,16 @@ final class ApplyForServiceTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //10
-        masters.count
+        typeOfServece.count
     }
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "specializationCell", for: indexPath)
        
-        let masters = masters[indexPath.row]
         var content = cell.defaultContentConfiguration()
          
-        //content.text = masters.fullName
-        content.secondaryText = masters.typeOfMaster
+        content.text = typeOfServece[indexPath.row]
         cell.contentConfiguration = content
         
         return cell
@@ -51,42 +49,6 @@ final class ApplyForServiceTableViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    
     // MARK: - Navigation
     
     
@@ -94,11 +56,11 @@ final class ApplyForServiceTableViewController: UITableViewController {
         guard let index = tableView.indexPathForSelectedRow else {return}
         guard let applyMasterVC = segue.destination as? ApplyMasterTableViewController else {return}
         
-        
-       // applyMasterVC.masters = masters[index.row]
+        let filtredMasters = masters.filter { $0.typeOfMaster == typeOfServece[index.row]
+        }
         
     }
-   
+    
 }
 
 
