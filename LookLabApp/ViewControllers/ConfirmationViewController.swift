f9a1167e1a3c2a49a8c065f7cbaa926c9b9e1815
Fedorwell <115953771+Fedorwell@users.noreply.github.com>
2023-06-05 22:49:18 +0300

    Add changes in DateTableVC, ConfirmVC and SuccessVC

--- LookLabApp/ViewControllers/ConfirmationViewController.swift
+++ LookLabApp/ViewControllers/ConfirmationViewController.swift
@@ -9,21 +9,25 @@ import UIKit
 
 final class ConfirmationViewController: UIViewController {
 
+    @IBOutlet var serviceLabel: UILabel!
+    @IBOutlet var masterLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var timeLabel: UILabel!
+    
+    @IBAction var confirmButton: UIButton!
+    
+    var master: Master!
+    var day: String!
+    var hour: String!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        serviceLabel.text = "Service: \(master.typeOfMaster)"
+        masterLabel.text = "Master: \(master.fullName)"
+        priceLabel.text = "Price: \(master.pricePerService)"
+        timeLabel.text = "Time \(dateLine)"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
