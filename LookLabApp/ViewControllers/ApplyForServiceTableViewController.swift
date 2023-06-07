15f3b9b0a9377ed377e024852ef3b67674bc5249
Fedorwell <115953771+Fedorwell@users.noreply.github.com>
2023-06-07 21:46:28 +0300

    Add images for ApplyForServiceTabelVC

--- LookLabApp/ViewControllers/ApplyForServiceTableViewController.swift
+++ LookLabApp/ViewControllers/ApplyForServiceTableViewController.swift
@@ -12,6 +12,13 @@ final class ApplyForServiceTableViewController: UITableViewController {
     var masters: [Master]!
     let typesOfServices = DataStore.shared.typeOfMaster
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 60
+        tableView.backgroundColor = .systemBrown
+        
+    }
+
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         typesOfServices.count
@@ -20,12 +27,26 @@ final class ApplyForServiceTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "specializationCell") else { return  UITableViewCell()}
         
+        let imageShaiving = "free-icon-clipper-3331475"
+        let imageHair = "free-icon-haircut-4428089"
+        let imageNail = "free-icon-nail-7029463"
+        
+        
         var content = cell.defaultContentConfiguration()
         content.text = typesOfServices[indexPath.row]
+        if typesOfServices[indexPath.item] == "Nail Service" {
+            content.image = UIImage(named: imageNail)
+        } else if typesOfServices[indexPath.item] == "Shaving" {
+            content.image = UIImage(named: imageShaiving)
+        } else if typesOfServices[indexPath.item] == "Hair Styling" {
+            content.image = UIImage(named: imageHair)
+        }
+        cell.backgroundColor = .systemBrown
         cell.contentConfiguration = content
         
         return cell
     }
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let masterSelectionVC = segue.destination as? ApplyMasterTableViewController else { return }
@@ -36,3 +57,6 @@ final class ApplyForServiceTableViewController: UITableViewController {
     }
     
 }
+    
+
+ 
