6baa07920a09f3285401933ec157aadeff6f713e
NetRunner <galatskiy@gmail.com>
2023-06-08 14:32:02 +0300

    Reload

--- LookLabApp/ViewControllers/TabBarController.swift
+++ LookLabApp/ViewControllers/TabBarController.swift
@@ -17,7 +17,7 @@ final class TabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         transferData()
-        delegate = self
+        //delegate = self
     }
     
     //MARK: - Private Methods
@@ -115,65 +115,65 @@ extension TabBarController: ConfirmationViewControllerDelegate {
 }
 
 //MARK: - UITabBarControllerDelegate
-extension TabBarController: UITabBarControllerDelegate {
-
-    func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return TabBarTransition(viewControllers: tabBarController.viewControllers)
-    }
-}
+//extension TabBarController: UITabBarControllerDelegate {
+//
+//    func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+//        return TabBarTransition(viewControllers: tabBarController.viewControllers)
+//    }
+//}
 
 //MARK: - TabBarTransition
-final class TabBarTransition: NSObject, UIViewControllerAnimatedTransitioning {
-
-    let viewControllers: [UIViewController]?
-    let transitionDuration: Double = 0.3
-
-    init(viewControllers: [UIViewController]?) {
-        self.viewControllers = viewControllers
-    }
-
-    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        return TimeInterval(transitionDuration)
-    }
-
-    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-
-        guard
-            let fromVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from),
-            let fromView = fromVC.view,
-            let fromIndex = getIndex(forViewController: fromVC),
-            let toVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to),
-            let toView = toVC.view,
-            let toIndex = getIndex(forViewController: toVC)
-            else {
-                transitionContext.completeTransition(false)
-                return
-        }
-
-        let frame = transitionContext.initialFrame(for: fromVC)
-        var fromFrameEnd = frame
-        var toFrameStart = frame
-        fromFrameEnd.origin.x = toIndex > fromIndex ? frame.origin.x - frame.width : frame.origin.x + frame.width
-        toFrameStart.origin.x = toIndex > fromIndex ? frame.origin.x + frame.width : frame.origin.x - frame.width
-        toView.frame = toFrameStart
-
-        DispatchQueue.main.async {
-            transitionContext.containerView.addSubview(toView)
-            UIView.animate(withDuration: self.transitionDuration, animations: {
-                fromView.frame = fromFrameEnd
-                toView.frame = frame
-            }, completion: {success in
-                fromView.removeFromSuperview()
-                transitionContext.completeTransition(success)
-            })
-        }
-    }
-
-    func getIndex(forViewController vc: UIViewController) -> Int? {
-        guard let vcs = self.viewControllers else { return nil }
-        for (index, thisVC) in vcs.enumerated() {
-            if thisVC == vc { return index }
-        }
-        return nil
-    }
-}
+//final class TabBarTransition: NSObject, UIViewControllerAnimatedTransitioning {
+//
+//    let viewControllers: [UIViewController]?
+//    let transitionDuration: Double = 0.3
+//
+//    init(viewControllers: [UIViewController]?) {
+//        self.viewControllers = viewControllers
+//    }
+//
+//    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+//        return TimeInterval(transitionDuration)
+//    }
+//
+//    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+//
+//        guard
+//            let fromVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from),
+//            let fromView = fromVC.view,
+//            let fromIndex = getIndex(forViewController: fromVC),
+//            let toVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to),
+//            let toView = toVC.view,
+//            let toIndex = getIndex(forViewController: toVC)
+//            else {
+//                transitionContext.completeTransition(false)
+//                return
+//        }
+//
+//        let frame = transitionContext.initialFrame(for: fromVC)
+//        var fromFrameEnd = frame
+//        var toFrameStart = frame
+//        fromFrameEnd.origin.x = toIndex > fromIndex ? frame.origin.x - frame.width : frame.origin.x + frame.width
+//        toFrameStart.origin.x = toIndex > fromIndex ? frame.origin.x + frame.width : frame.origin.x - frame.width
+//        toView.frame = toFrameStart
+//
+//        DispatchQueue.main.async {
+//            transitionContext.containerView.addSubview(toView)
+//            UIView.animate(withDuration: self.transitionDuration, animations: {
+//                fromView.frame = fromFrameEnd
+//                toView.frame = frame
+//            }, completion: {success in
+//                fromView.removeFromSuperview()
+//                transitionContext.completeTransition(success)
+//            })
+//        }
+//    }
+//
+//    func getIndex(forViewController vc: UIViewController) -> Int? {
+//        guard let vcs = self.viewControllers else { return nil }
+//        for (index, thisVC) in vcs.enumerated() {
+//            if thisVC == vc { return index }
+//        }
+//        return nil
+//    }
+//}
